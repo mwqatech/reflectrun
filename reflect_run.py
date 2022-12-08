f43f43e39eec28ed3ebdcb5dbe33d17537b240cc
mwqatech <120082055+mwqatech@users.noreply.github.com>
2022-12-08 12:05:23 +0530

    Add files via upload

---
+++ reflect_run.py
@@ -0,0 +1,29 @@
+import requests
+import json
+
+data = {}
+
+url = "https://api.reflect.run/v1/tests/71066/executions"
+
+payload = ""
+headers = {"x-api-key": "x1OBUoR7PY4qH4RyH199pwuN1a7ofw32BxmrfSxf"}
+
+response = requests.request("POST", url, data=payload, headers=headers)
+json_data = json.loads(response.text)
+
+url2 = "https://api.reflect.run/v1/executions/" + str(json_data['executionId'])
+
+while True:
+    response2 = requests.request("GET", url2, data=payload, headers=headers)
+    data = json.loads(response2.text)
+    print(data['tests'][0]['status'])
+    if(data['tests'][0]['status'] != 'running'):
+        break
+
+if(data['tests'][0]['status'] == 'failed'):
+    print("Test Execution Failed\n")
+
+
+
+
+
