29158dcf9073f2901d5ec167d5610a8432e59b9a
mwqatech <120082055+mwqatech@users.noreply.github.com>
2022-12-08 21:30:23 +0530

    Update reflect_run_bw.py

--- reflect_run_bw.py
+++ reflect_run_bw.py
@@ -1,1 +1,27 @@
+import requests
+import json
+import sys
 
+data = {}
+
+url = "https://api.reflect.run/v1/tests/81208/executions"
+print(url)
+
+payload = ""
+headers = {"x-api-key": "x1OBUoR7PY4qH4RyH199pwuN1a7ofw32BxmrfSxf"}
+
+response = requests.request("POST", url, data=payload, headers=headers)
+json_data = json.loads(response.text)
+
+url2 = "https://api.reflect.run/v1/executions/" + str(json_data['executionId'])
+
+while True:
+    response2 = requests.request("GET", url2, data=payload, headers=headers)
+    data = json.loads(response2.text)
+    print(data['tests'][0]['status'])
+    if((data['tests'][0]['status'] != 'running') and (data['tests'][0]['status'] != 'queued')):
+        break
+
+if(data['tests'][0]['status'] == 'failed'):
+    print("Test Execution Failed\n")
+    sys.exit(-1)
