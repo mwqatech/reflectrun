bdc63884818d51f9615e1ce990f7c29adf374621
mwqatech <120082055+mwqatech@users.noreply.github.com>
2022-12-13 21:15:17 +0530

    Update reflect_run_mm.py

--- reflect_run_mm.py
+++ reflect_run_mm.py
@@ -20,22 +20,31 @@ for key, value in yaml.load(open('testdata.yml'), Loader=SafeLoader).items():
             headers = {"x-api-key": "x1OBUoR7PY4qH4RyH199pwuN1a7ofw32BxmrfSxf"}
 
             response = requests.request("POST", url, data=payload, headers=headers)
-            json_data = json.loads(response.text)
+            if(response.status_code == 200):
+                json_data = json.loads(response.text)
 
-            url2 = "https://api.reflect.run/v1/executions/" + str(json_data['executionId'])
+                url2 = "https://api.reflect.run/v1/executions/" + str(json_data['executionId'])
 
-            while True:
-                response2 = requests.request("GET", url2, data=payload, headers=headers)
-                data = json.loads(response2.text)
-                print(data['tests'][0]['status'])
-                if((data['tests'][0]['status'] != 'running') and (data['tests'][0]['status'] != 'queued')):
-                    break
+                while True:
+                    response2 = requests.request("GET", url2, data=payload, headers=headers)
+                    data = json.loads(response2.text)
+                    print(data['tests'][0]['status'])
+                    if((data['tests'][0]['status'] != 'running') and (data['tests'][0]['status'] != 'queued')):
+                        break
 
-            if(data['tests'][0]['status'] == 'failed'):
-                print("Test Execution:"+ str(tvalue) +" is Failed\n")
+                if(data['tests'][0]['status'] == 'failed'):
+                    print("Test Execution:"+ str(tvalue) +" is Failed\n")
+                    failed_tcs.append(tvalue)
+
+                if(len(failed_tcs) != 0 and total_tcs == 0):
+                    print("Please check the failed/not triggered TCs:", failed_tcs)
+                    system.exit(-1)
+                total_tcs -= 1
+             else:
+                print("Test Execution for :"+ str(tvalue) +" is not triggered\n")
                 failed_tcs.append(tvalue)
-                
-            if(len(failed_tcs) != 0 and total_tcs == 0):
-                print("Please check the failed TCs:", failed_tcs)
-                system.exit(-1)
-            total_tcs -= 1
+
+                if(len(failed_tcs) != 0 and total_tcs == 0):
+                    print("Please check the failed/not triggered TCs:", failed_tcs)
+                    system.exit(-1)
+                total_tcs -= 1
