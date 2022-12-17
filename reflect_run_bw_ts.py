3c40ed3e0b1cf47e7c576414a17a889ffe03c2dd
mwqatech <120082055+mwqatech@users.noreply.github.com>
2022-12-17 17:04:28 +0530

    Update reflect_run_bw_ts.py

--- reflect_run_bw_ts.py
+++ reflect_run_bw_ts.py
@@ -5,48 +5,49 @@ import yaml
 from yaml.loader import SafeLoader
 
 data = {}
-total_tcs = 0
-passed_tcs = []
-failed_tcs = []
-for key, value in yaml.load(open('testdata.yml'), Loader=SafeLoader).items():
-    if(str(key) == 'BW_Testdata'):
-        total_tcs = len(value)
+total_tss = 0
+passed_tss = []
+failed_tss = []
+for key, value in yaml.load(open('testdata_ts.yml'), Loader=SafeLoader).items():
+    if (str(key) == 'BW'):
+        total_tss = len(value)
         for tkey, tvalue in value.items():
-            print("Testcase ID = ",tvalue)
-            url = "https://api.reflect.run/v1/tests/" + str(tvalue) + "/executions"
+            print("Testcase ID = ", tvalue)
+            url = "https://api.reflect.run/v1/suites/" + str(tvalue) + "/executions"
             print(url)
 
             payload = ""
             headers = {"x-api-key": "j5rPTzYWK16hh7xyPRsnz1EYDIbyAcUE7sOKNp4U"}
 
             response = requests.request("POST", url, data=payload, headers=headers)
-            if(response.status_code == 200):
-                total_tcs -= 1
+            if (response.status_code == 200):
+                total_tss -= 1
                 json_data = json.loads(response.text)
 
-                url2 = "https://api.reflect.run/v1/executions/" + str(json_data['executionId'])
+                url2 = "https://api.reflect.run/v1/suites/" + str(tvalue) + "/executions/" + str(json_data['executionId'])
 
                 while True:
-                    response2 = requests.request("GET", url2, data=payload, headers=headers)
+                    response2 = requests.request("GET", url2, headers=headers)
                     data = json.loads(response2.text)
-                    print(data['tests'][0]['status'])
-                    if((data['tests'][0]['status'] != 'running') and (data['tests'][0]['status'] != 'queued')):
+                    print(data['status'])
+                    print(data['isFinished'])
+                    if ((data['status'] != 'pending') and (data['status'] != 'running') and (data['isFinished'] != False)):
                         break
 
-                if(data['tests'][0]['status'] == 'failed'):
-                    print("Test Execution of TC ID "+ str(tvalue) +" is Failed\n")
-                    failed_tcs.append(tvalue)
-                    print("Total_TCs to be executed", total_tcs)
+                if (data['status'] == 'failed'):
+                    print("Test Suite Execution of Test Suite ID " + str(tvalue) + " is Failed\n")
+                    failed_tss.append(tvalue)
+                    print("Total Test Suites to be executed", total_tss)
 
-                if(len(failed_tcs) != 0 and total_tcs == 0):
-                    print("Please check the failed/not triggered TCs:", failed_tcs)
+                if (len(failed_tss) != 0 and total_tss == 0):
+                    print("Please check the failed/not triggered TCs:", failed_tss)
                     sys.exit(-1)
             else:
-                total_tcs -= 1
-                print("Test Execution of TC ID "+ str(tvalue) +" is not triggered\n")
-                failed_tcs.append(tvalue)
-                print("Total_TCs to be executed", total_tcs)
-          
-                if(len(failed_tcs) != 0 and total_tcs == 0):
-                    print("Please check the failed/not triggered TCs:", failed_tcs)
+                total_tss -= 1
+                print("Test Execution of TC ID " + str(tvalue) + " is not triggered\n")
+                failed_tss.append(tvalue)
+                print("Total_TCs to be executed", total_tss)
+
+                if (len(failed_tss) != 0 and total_tss == 0):
+                    print("Please check the failed/not triggered TCs:", failed_tss)
                     sys.exit(-1)
